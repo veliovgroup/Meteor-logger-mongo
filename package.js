500aba5ed35f32a4a9e103ea823af60818169735
dr.dimitru <ceo@veliov.com>
2015-03-30 18:53:08 +0300

    v0.0.4

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   name: 'ostrio:loggermongo',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Simply store application logs into MongoDB within ostrio:logger package',
   git: 'https://github.com/VeliovGroup/Meteor-logger-mongo',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
-  api.use('ostrio:logger@0.0.2', ['client', 'server']);
-  api.use('coffeescript', ['client', 'server']);
+  api.versionsFrom('1.0');
+  api.use(['ostrio:logger@0.0.4', 'coffeescript'], ['client', 'server']);
   api.addFiles('ostrio:loggermongo.coffee', ['client', 'server']);
 });
