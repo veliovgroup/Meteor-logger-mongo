5851998d8e1924b6b8ae1b50a97138c35cb1a675
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-05-25 02:40:34 +0300

    v0.0.6
     - Deps packages update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:loggermongo',
-  version: '0.0.5',
+  version: '0.0.6',
   summary: 'Simply store application logs into MongoDB within ostrio:logger package',
   git: 'https://github.com/VeliovGroup/Meteor-logger-mongo',
   documentation: 'README.md'
@@ -8,6 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['ostrio:logger@0.0.4', 'coffeescript'], ['client', 'server']);
+  api.use(['ostrio:logger@0.0.5', 'coffeescript'], ['client', 'server']);
   api.addFiles('ostrio:loggermongo.coffee', ['client', 'server']);
 });
