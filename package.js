8155db0a7fa56a4d8f22a4d1033fcc56d3a87b50
Dmitriy A. Golev <dr.dimitru@gmail.com>
2015-05-29 01:23:40 +0300

    v0.0.7
     - Remove colon from file names, to avoid Windows compilation issues
     - License update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:loggermongo',
-  version: '0.0.6',
+  version: '0.0.7',
   summary: 'Simply store application logs into MongoDB within ostrio:logger package',
   git: 'https://github.com/VeliovGroup/Meteor-logger-mongo',
   documentation: 'README.md'
@@ -8,6 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['ostrio:logger@0.0.5', 'coffeescript'], ['client', 'server']);
-  api.addFiles('ostrio:loggermongo.coffee', ['client', 'server']);
+  api.use(['ostrio:logger@0.0.6', 'coffeescript'], ['client', 'server']);
+  api.addFiles('loggermongo.coffee', ['client', 'server']);
 });
