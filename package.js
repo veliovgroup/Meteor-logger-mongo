3ce611c7f39465b59c7b641f2b77afcd629e68b0
dr.dimitru <ceo@veliov.com>
2017-08-01 20:48:25 +0300

    v2.0.1
     - Compatibility with `meteor@1.5.1`
     - Dependencies update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:loggermongo',
-  version: '2.0.0',
+  version: '2.0.1',
   summary: 'Logging: Store application\'s logs messages in MongoDB (Server & Client support)',
   git: 'https://github.com/VeliovGroup/Meteor-logger-mongo',
   documentation: 'README.md'
@@ -9,12 +9,12 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
   api.use('mongo', 'server');
-  api.use(['mongo', 'ecmascript', 'check', 'underscore', 'ostrio:logger@2.0.2'], ['client', 'server']);
+  api.use(['mongo', 'ecmascript', 'check', 'underscore', 'ostrio:logger@2.0.3'], ['client', 'server']);
   api.mainModule('loggermongo.js', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use(['ecmascript', 'underscore', 'ostrio:logger@2.0.2', 'ostrio:loggermongo@2.0.0']);
+  api.use(['ecmascript', 'underscore', 'ostrio:logger@2.0.3', 'ostrio:loggermongo@2.0.1']);
   api.addFiles('loggermongo-tests.js');
 });
