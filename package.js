0faf1955c2cf915cc0f8f0322dcb214bc05a1173
dr.dimitru <ceo@veliov.com>
2018-07-27 03:23:45 +0300

    v2.0.4
     - 🗑 Get rid of `underscore` package
     - 📦 Update Atmosphere dependencies
     - 🤝 Compatibility with `meteor@1.7.0.3`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:loggermongo',
-  version: '2.0.3',
+  version: '2.0.4',
   summary: 'Logging: Store application\'s logs messages in MongoDB (Server & Client support)',
   git: 'https://github.com/VeliovGroup/Meteor-logger-mongo',
   documentation: 'README.md'
@@ -9,12 +9,12 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
   api.use('mongo', 'server');
-  api.use(['mongo', 'ecmascript', 'check', 'underscore', 'ostrio:logger@2.0.5'], ['client', 'server']);
+  api.use(['mongo', 'ecmascript', 'check', 'ostrio:logger@2.0.7'], ['client', 'server']);
   api.mainModule('loggermongo.js', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use(['ecmascript', 'underscore', 'ostrio:logger@2.0.5', 'ostrio:loggermongo@2.0.3']);
+  api.use(['ecmascript', 'underscore', 'ostrio:logger', 'ostrio:loggermongo']);
   api.addFiles('loggermongo-tests.js');
 });
